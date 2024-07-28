f487c73824633a14932d3fb86c3770ca73afe707
unix-waltz <riosraskaa@gmail.com>
2024-07-28 13:32:45 +0700

    deploy

--- apps/Utility/cookie.utils.js
+++ apps/Utility/cookie.utils.js
@@ -1,14 +1,14 @@
 const setcookie = ({res,token})=>{
     res.cookie("_token", token, {
         path: "/",
-        sameSite: "strict",
+        sameSite: "Strict",
         httpOnly: true,
       });
 }
 const deletecookie = (res)=>{
     res.clearCookie("_token", {
         path: "/",
-        sameSite: "strict",
+        sameSite: "Strict",
         httpOnly: true,
       });
 }
